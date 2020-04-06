9e3c8dd8c96a4e3e85beb046a2b514ec8fd3f194
Konstantin Bulenkov <kb@jetbrains.com>
2020-04-06 13:34:29 +0200

    Generate JetBrains Mono NL files automatically

--- scripts/UpdateMetaData.kt
+++ scripts/UpdateMetaData.kt
@@ -4,7 +4,6 @@ import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import java.io.File
 import java.io.FileOutputStream
-import java.nio.file.Files
 import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.TransformerFactory
@@ -27,28 +26,31 @@ import javax.xml.xpath.XPathFactory
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 fun main() {
     File("./ttf/")
-            .listFiles { _, name -> name.endsWith(".ttf") }
+            .listFiles { _, name -> name.endsWith(".ttf") && !name.startsWith("JetBrainsMonoNL") }
             .forEach {
                 val ttx = it.nameWithoutExtension + ".ttx"
                 val dir = it.parentFile
                 File(dir, ttx).deleteAndLog()
-                updateMetaData(it)
+                val doc = updateMetaData(it)
                 it.deleteAndLog()
                 "ttx $ttx".runCommand(dir)
                 File(dir, ttx).deleteAndLog()
+                if (doc != null) {
+                    generateNoLigaturesFont(File(dir, it.name), doc)
+                }
             }
 }
 
-fun updateMetaData(file: File) {
+fun updateMetaData(file: File): Document? {
     "ttx ${file.name}".runCommand(file.parentFile)
     val ttx = file.parentFile.listFiles { _, name -> name == file.nameWithoutExtension + ".ttx" }?.first()
-    if (ttx == null) return
+    if (ttx == null) return null
     val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
     val doc = documentBuilder.parse(ttx)
     updateMonospaceFlags(file, doc)
     updateNameRecords(file, doc)
-    val transformer = TransformerFactory.newInstance().newTransformer()
-    transformer.transform(DOMSource(doc), StreamResult(FileOutputStream(ttx)))
+    doc.saveAs(ttx)
+    return doc
 }
 
 fun updateNameRecords(file: File, doc: Document) {
@@ -85,7 +87,35 @@ fun updateNameRecords(file: File, doc: Document) {
     }
 }
 
-fun NodeList.asList(): List<Node> = NodeListWrapper(this)
+fun generateNoLigaturesFont(file: File, doc: Document) {
+    val nlName = file.nameWithoutExtension.replace("JetBrainsMono", "JetBrainsMonoNL")
+    val ttx = nlName + ".ttx"
+    val ttf = nlName + ".ttf"
+    val dir = file.parentFile
+    File(dir, ttf).deleteAndLog()
+    doc.removeLigas("/ttFont/GlyphOrder", "GlyphID")
+    doc.removeLigas("/ttFont/glyf", "TTGlyph")
+    doc.removeLigas("/ttFont/hmtx", "mtx")
+    doc.removeLigas("/ttFont/post/extraNames", "psName")
+    doc.removeLigas("/ttFont/GDEF/GlyphClassDef", "ClassDef", attName = "glyph")
+    doc.removeNode("/ttFont/GPOS")
+    doc.removeNode("/ttFont/GSUB")
+
+    val xPath = XPathFactory.newInstance().newXPath()
+    val nameRecords = (xPath.evaluate("/ttFont/name/namerecord", doc, XPathConstants.NODESET) as NodeList).asList()
+    nameRecords.forEach {
+        if (!it.textContent.contains("trademark")) {
+            it.textContent = it.textContent
+                .replace("JetBrains Mono", "JetBrains Mono NL")
+                .replace("JetBrainsMono", "JetBrainsMonoNL")
+        }
+    }
+
+    val ttxFile = File(dir, ttx)
+    doc.saveAs(ttxFile)
+    "ttx $ttx".runCommand(dir)
+    ttxFile.deleteAndLog()
+}
 
 class NodeListWrapper(val nodeList: NodeList) : AbstractList<Node>(), RandomAccess {
     override val size: Int
@@ -108,6 +138,8 @@ private fun updateMonospaceFlags(file: File, doc: Document) {
 
 ////////////////////// Utility functions and data classes //////////////////////
 
+fun NodeList.asList(): List<Node> = NodeListWrapper(this)
+
 fun String.runCommand(workingDir: File) {
     ProcessBuilder(*split(" ").toTypedArray())
         .directory(workingDir)
@@ -115,6 +147,26 @@ fun String.runCommand(workingDir: File) {
         .redirectError(ProcessBuilder.Redirect.INHERIT)
         .start()
         .waitFor(1, TimeUnit.MINUTES)
+}
+
+fun Document.saveAs(file: File) {
+    val transformer = TransformerFactory.newInstance().newTransformer()
+    transformer.transform(DOMSource(this), StreamResult(FileOutputStream(file)))
+}
+
+fun Document.removeLigas(parentPath: String, nodeName: String, attName:String = "name") {
+    val xPath = XPathFactory.newInstance().newXPath()
+    val parent = xPath.evaluate(parentPath, this, XPathConstants.NODE) as Node
+    val nodeFilter = "$parentPath/$nodeName[substring(@$attName, string-length(@$attName)-4) = '.liga']"
+    val nodes = (xPath.evaluate(nodeFilter, this, XPathConstants.NODESET) as NodeList).asList()
+    nodes.forEach { parent.removeChild(it) }
+}
+
+fun Document.removeNode(path: String) {
+    val xPath = XPathFactory.newInstance().newXPath()
+    val parent = xPath.evaluate(path.substringBeforeLast("/"), this, XPathConstants.NODE) as Node
+    val child = xPath.evaluate(path, this, XPathConstants.NODE) as Node
+    parent.removeChild(child)
 }
 
 fun File.deleteAndLog() {
