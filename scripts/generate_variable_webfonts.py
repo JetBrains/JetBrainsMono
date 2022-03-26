e02c9cac2901b180aa556375fb92c50011df80fa
Naiyer Asif <19614213+naiyerasif@users.noreply.github.com>
2022-03-26 13:54:02 +0530

    Print status during WOFF2 generation

--- scripts/generate_variable_webfonts.py
+++ scripts/generate_variable_webfonts.py
@@ -5,4 +5,5 @@ from fontTools.ttLib import TTFont
 for filepath in glob.iglob('fonts/variable/*.ttf'):
 	f = TTFont(filepath)
 	f.flavor = 'woff2'
+	print('INFO:fontTools.ttLib.woff2:Building WOFF2 for ' + filepath)
 	f.save(os.path.splitext(filepath)[0] + '.woff2')
